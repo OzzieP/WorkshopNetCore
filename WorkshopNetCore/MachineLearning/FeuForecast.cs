69166b08229dd60ee0a57b0507b7c111b2d75756
Brendan LEGENDRE <brendan.legendre@epsi.fr>
2020-11-19 18:35:37 +0100

    Machine Learning

---
+++ WorkshopNetCore/MachineLearning/FeuForecast.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkshopNetCore.MachineLearning
+{
+    public class FeuForecast
+    {
+        public string Jour { get; set; }
+
+        public float Semaine { get; set; }
+
+        public float PassantsActuel { get; set; }
+
+        public float EstimationInferieure { get; set; }
+
+        public float EstimationSuperieure { get; set; }
+
+        public float Estimation { get; set; }
+
+        public FeuForecast()
+        {
+
+        }
+
+        public FeuForecast(string jour, float semaine, float passantsActuel, float estimationInferieure, float estimation, float estimationSuperieure)
+        {
+            Jour = jour;
+            Semaine = semaine;
+            PassantsActuel = passantsActuel;
+            EstimationInferieure = estimationInferieure;
+            Estimation = estimation;
+            EstimationSuperieure = estimationSuperieure;
+        }
+    }
+}
